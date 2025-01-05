09dcb8a406d8f53fe883b2bf7ca882f54d469c37
Ayeshaazafarr <ayeshazafarr.0@gmail.com>
2025-01-05 22:44:34 +0500

    push 6

---
+++ 2.3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int num1 , num2;
+	
+	cout<<"Enter 1st number";
+	cin>>num1;
+	cout<<"Enter 2nd number";
+	cin>>num2;
+	
+	if(num1%2==0 && num2 %2==0){
+		cout<<"The numbers are even";
+	}else {
+		
+	cout<<"The numbers are odd";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
