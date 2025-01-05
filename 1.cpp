f3355fd0b7b56b921049662a2bd2f53c2575adbf
Ayeshaazafarr <ayeshazafarr.0@gmail.com>
2025-01-05 21:58:53 +0500

    push 6

---
+++ 1.cpp
@@ -0,0 +1,33 @@
+//variable is a container that is used To store some value
+//naming a variable is called decalaration
+//assigning a value is called initialization
+//basic types are int,char,float,double,bool
+//int is used tO STore numerical positive value.(no sign )
+//char is used to store characters
+//double is used to store floating point values upto 15 digitS
+//float is used to store decimal values
+//bool is used to validate a condition whETher true or false
+//const is used for fixed values
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i; //declare
+	i=7; //initiualize
+	char ch;//declare
+	ch='A';//intialize
+	float flo;//declare
+	flo=0.24;//initialize
+	double num;
+	num=899.00;
+	bool istrue;
+	istrue=true;
+	cout<<i<<endl;
+	cout<<ch<<endl;
+	cout<<flo<<endl;
+	cout<<num<<endl;
+	cout<<istrue<<endl;
+
+		return 0;
+}
